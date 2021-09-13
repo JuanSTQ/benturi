c00ec8927c743f1c070c88901f972e94d97eb26a
juanstq <juanstq20@gmail.com>
2021-09-13 16:37:14 -0500

    Initial Config

---
+++ src/containers/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Home = () => (
+  <>
+    <h4>ESTO ES UN H4</h4>
+    <form action="">
+      <button type="button">Clickme</button>
+    </form>
+  </>
+);
+
+export default Home;
